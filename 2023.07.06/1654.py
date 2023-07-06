746ebcf2d3536ec3665f05c343a54060058cae4a
KimKyuHoi <k546kh@gmail.com>
2023-07-06 20:05:11 +0900

    Feat: 2023.07.06 백준 알고리즘(이진 탐색)

---
+++ 2023.07.06/1654.py
@@ -0,0 +1,28 @@
+#1654번
+#https://www.acmicpc.net/problem/2751
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+array =[]
+
+for i in range(n):
+    array.append(int(input()))
+
+start = 1
+end = max(array)
+
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for x in array:
+       
+        total += x // mid
+        
+            
+    if total >= k:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
---
+++ 2023.07.06/1790.py
@@ -0,0 +1,26 @@
+#1790번
+#https://www.acmicpc.net/problem/2751
+
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+last_num = 0
+num_len = 1
+num_count = 9
+
+while k> num_len * num_count:
+    k -= num_len * num_count
+    last_num += num_count
+    num_len += 1
+    num_count *= 10
+    
+    
+last_num  = (last_num + 1) +((k-1)//num_len)
+
+if last_num > n:
+    print(-1)
+else:
+   print(str(last_num)[(k-1)%num_len]) 
